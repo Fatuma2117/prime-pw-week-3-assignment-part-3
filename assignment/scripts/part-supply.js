a704c0dd747a8d2d7dfff5af883d6eaf3fb0217a
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-03-10 19:19:06 -0600

    Edits

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -10,7 +10,7 @@ console.log('Expect 40', partsNeeded);
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
-let supplyChanges = ['3','5','0','7','11']
+let supplyChanges = [3,5,0,7,11]
 console.log('Expect the list of numbers', supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
@@ -25,26 +25,16 @@ console.log(' Expect number 11 removed:', supplyChanges.pop());
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
-supplyChanges.push('25')
+supplyChanges.push(25)
 console.log(supplyChanges);
+
+
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-for (let i = 0; i<supplyChanges.length; i++){
-    if (i > 0) {
-        console.log('Added x parts');
-    };
-    // like the practical example match
-    // x = value from supplyChanges
-    // if i>0 , log 'added (positive number) parts
-}
-
-
-
-
 
 
 // STRETCH GOALS
@@ -55,6 +45,12 @@ console.log('7. Showing supplyChanges with "for of" loop');
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let total = 0
+for (let i = 0; i < supplyChanges.length; i++) {
+     total += supplyChanges[i];  
+}
+console.log('Expect total of 40:' , total);
+
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -63,3 +59,10 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+let partsCount = 572
+let boxesFilled = 0
+while (partsCount >= 7) {
+    partsCount -= 7;
+    boxesFilled += 1;
+    console.log(`Number of boxes filled: ${boxesFilled}, left over parts ${partsCount}`);
+}
